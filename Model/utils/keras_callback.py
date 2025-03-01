06313b75dbde716580ab7402ceebb1167042b801
TheTrooper83 <73314358+TheTrooper83@users.noreply.github.com>
2025-03-01 10:54:40 +0000

    Completed Learning Rate trials

--- Model/utils/keras_callback.py
+++ Model/utils/keras_callback.py
@@ -23,3 +23,36 @@ class TimeHistory(keras.callbacks.Callback):
 
     def on_epoch_end(self, batch, logs={}):
         self.times.append(time.time() - self.epoch_time_start)
+
+
+class CLR_history(keras.callbacks.Callback):
+  
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.accuracy = []
+
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.accuracy.append(logs.get('accuracy'))
+    
+    # def on_epoch_end(self, epoch, logs={}):
+    #     self.losses.append(logs.get('loss'))
+    #     self.accuracy.append(logs.get('accuracy'))
+
+
+    # def on_test_batch_end(self, batch, logs={}):        
+    #     self.val_losses.append(logs.get('val_loss'))        
+    #     self.val_accuracy.append(logs.get('val_accuracy'))
+
+    # def on_epoch_end(self, batch, logs={}):
+    #     self.lr_rate.append(self.model.optimizer.lr)
+    
+class stop_model(keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.epoch = epoch + 1
+
+        if self.epoch == 25:
+            self.model.stop_training = True
+              
